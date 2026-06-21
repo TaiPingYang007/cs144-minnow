bec959582b43677bb4dff4c8a10a0620a3a6075f
TaiPingYang007 <1421719536@qq.com>
2026-06-21 21:20:42 +0800

    lab6

--- src/router.cc
+++ src/router.cc
@@ -2,6 +2,7 @@
 #include "debug.hh"
 
 #include <iostream>
+#include <map>
 
 using namespace std;
 
@@ -16,15 +17,51 @@ void Router::add_route( const uint32_t route_prefix,
                         const optional<Address> next_hop,
                         const size_t interface_num )
 {
+  routing_table_.push_back( { route_prefix, prefix_length, next_hop, interface_num } );
+
   cerr << "DEBUG: adding route " << Address::from_ipv4_numeric( route_prefix ).ip() << "/"
        << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
        << " on interface " << interface_num << "\n";
-
-  debug( "unimplemented add_route() called" );
 }
 
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
-  debug( "unimplemented route() called" );
+  // 获取目的IP + 检查TTL（如果为0就丢弃） + 查路由表（获取下一跳IP） + TTL-1 + 发送
+  for ( auto& interface_ : interfaces_ ) {
+    // 获取目的IP
+    while ( !interface_->datagrams_received().empty() ) {
+      uint32_t dst_ip = interface_->datagrams_received().front().header.dst;
+
+      // 检查TTL（如果<= 1就丢弃）
+      if ( interface_->datagrams_received().front().header.ttl <= 1 ) {
+        interface_->datagrams_received().pop();
+        continue;
+      }
+
+      // 查路由表
+      const RouteEntry* best_route = nullptr; // 目前最优,初始没有
+      for ( auto& route : routing_table_ ) {
+        uint32_t mask = ( route.prefix_length == 0 ) ? 0u : ( ~0u << ( 32 - route.prefix_length ) ); // 创造掩码
+        // 检查是否匹配
+        if ( ( route.route_prefix & mask ) == ( dst_ip & mask ) )
+          if ( best_route == nullptr || route.prefix_length > best_route->prefix_length ) {
+            best_route = &route;
+          }
+      }
+      // 找到route_map中prefix_length最大的一条路由并发送，如果没有就丢弃
+      if ( best_route != nullptr ) {
+        interface_->datagrams_received().front().header.ttl--;
+        InternetDatagram dgram = interface_->datagrams_received().front();
+        dgram.header.compute_checksum();
+        Address actual_next_hop
+          = best_route->next_hop.has_value()
+              ? best_route->next_hop.value()
+              : Address::from_ipv4_numeric( dgram.header.dst ); // 如果为空，就用包自己的目的IP构造Addres
+
+        interfaces_[best_route->interface_num]->send_datagram( dgram, actual_next_hop );
+      }
+      interface_->datagrams_received().pop();
+    }
+  }
 }
--- src/router.hh
+++ src/router.hh
@@ -40,4 +40,14 @@ public:
 private:
   // The router's collection of network interfaces
   std::vector<std::shared_ptr<NetworkInterface>> interfaces_ {};
+
+  struct RouteEntry
+  {
+    uint32_t route_prefix {};
+    uint8_t prefix_length {};
+    std::optional<Address> next_hop {};
+    size_t interface_num {};
+  };
+
+  std::vector<RouteEntry> routing_table_ {};
 };
